4280771a469d1882235cc629fbc67b2d104fe8fa
mulveyops <mulveyops@gmail.com>
2025-06-17 11:11:53 -0500

    added Hanna's image and updated stylist data

--- client/src/data/stylists.ts
+++ client/src/data/stylists.ts
@@ -45,5 +45,14 @@ export const stylists: Stylist[] = [
     specialties: ["Blonding and Brightening", "Comfort Centered Styling", "Dimensional Color"],
     bookingUrl: "https://heirloomstudiomke.glossgenius.com/",
     instagramUrl: "https://www.instagram.com/hairbyannavg?igsh=aGd6OWVqaWY0Njlh"
+  },
+    {
+    name: "Hanna",
+    title: "Stylist",
+    image: "images/Hanna/.jpg",
+    bio: "Hanna has been a passionate hairstylist for over 5 years, specializing in men’s barbering and long layered haircuts. With a sharp eye for detail and a love for both precision and creativity, she’s all about helping clients look and feel their best.  What sets Hanna apart is the welcoming, comfortable vibe she creates in her chair. She believes a great haircut starts with trust and a good conversation. Whether it’s a clean fade or effortless layers, Hanna brings skill, warmth, and care to every appointment.",
+    specialties: ["Barbering", "Long Layered Cuts"],
+    bookingUrl: "https://heirloomstudiomke.glossgenius.com/",
+    instagramUrl: "https://www.instagram.com/hairbyhannamke?igsh=MW11NWw2MW5yb2F1ZA=="
   }
 ];
